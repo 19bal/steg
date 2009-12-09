034f236eb36c1617defae47a211210cc8ff4c482
unknown <yonetici@.(none)>
2009-12-09 16:31:49 +0200

    re_BinBl eklendi

---
+++ src/re_BinBl.m
@@ -0,0 +1,27 @@
+function tB = re_BinBl(rBl)
+%function tB = re_BinBl(rBl)
+%
+%re_BinBl rBl blogunun icerisine bulunan 1 byte' lik
+%gizli veriyi alir ve tB byte' inin icine atarak
+%geri dondurur.
+%
+%   Ornek Kullanim
+%   -------
+%   rBl = magic(3);
+%   tB = re_BinBl(rBl)
+%   
+% 
+%   See also st_binB
+
+% $Date: 2009/12/09 16:25 $
+
+for i=1:3
+    for j=1:3
+        if i == 1 & j == 1, continue; end
+		
+		temp = rBl(i, j);%Blogun icindeki 9 kareden sadece biri
+        temp = dec2bin(temp, 8);%Karedeki sayiyi binary olarak yaz
+		temp2(1,i*3+j) = temp(8);%Binary olarak yazdigin sayinin sadece en son bitini al.
+    end
+end
+tB = uint8(bin2dec(temp2));
