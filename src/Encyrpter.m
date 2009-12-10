343e8093c1d69c6a839727cc43dd4c0f2736f934
unknown <yonetici@.(none)>
2009-12-10 14:32:47 +0200

    10.12.2009 saat 14.32

--- src/Encyrpter.m
+++ src/Encyrpter.m
@@ -7,11 +7,12 @@ StegoGray = rgb2gray(StegoImage);%Simdilik gri tonajli resimler uzerinde calisal
 BlockSize = 3;%Bloklarimiz "BlockSize x BlockSize" ebadinda kucuk matrislerden olusacak.
 BlocksInWidth = (size(StegoImage, 2) - mod(size(StegoImage,2), BlockSize))/BlockSize;%Resme soldan saga en fazla kac blok sigdirilabilecegi
 BlocksInHeight = (size(StegoImage, 1) - mod(size(StegoImage,1), BlockSize))/BlockSize;%Resme yukaridan asagiya en fazla kac blok sigdirilabilecegi
-
+CurrentBlock = zeros(BlockSize);%Resimden blok okudukca
 % dec2bin(uint8(StegoData(2)), 8) %Tek bir karakteri, bir bloga yerlestirirken kullanacagiz.
 
 for i=1:BlocksInHeight
-   for j=1:BlocksInWidth
+   for j=1:BlocksInWidth 
+       %Burada, "(i-1)*BlocksInWidth + j" degeri, bize kacinci blokta oldugumuzu verecektir.
        
    end
 end
