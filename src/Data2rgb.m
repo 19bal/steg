7a335d8e8b1a737a947b2846178d3feef7effbec
Nurettin Senyer <seyyah@bil.omu.edu.tr>
2010-12-17 17:00:25 +0200

    jpg yerine SIKISTIRMA KULLANMAYAN PNG | BMP kullan

--- src/Data2rgb.m
+++ src/Data2rgb.m
@@ -1,5 +1,5 @@
-function Data2rgb(ImPath, data, stImPath)
-% function Data2rgb(ImPath, data, stImPath)
+function Data2rgb(ImPath, data, stImPath, renk)
+% function Data2rgb(ImPath, data, stImPath, renk)
 %
 % Icerisine veri gizlenecek olan resim dosyasinin adresini,
 % gizlenecek olan veriyi, ve veri gizlenmis olan resmin
@@ -11,21 +11,18 @@ function Data2rgb(ImPath, data, stImPath)
 %   kaynak_resim = 'manzara.bmp';
 %   veri = 'omu.edu.tr';
 %   hedef_resim = 'sonuc.jpg';
-%   Data2rgb(kaynak_resim, veri, hedef_resim);
+%   Data2rgb(kaynak_resim, veri, hedef_resim, 1);
 %
 % Yukaridaki ornek calistirildiktan sonra, calisma dizinimizde,
 % icerisine 'omu.edu.tr' verisi gizlenmis olan, 'sonuc.jpg'
 % isminde bir resim dosyasi olusacaktir.
-
-renk = 1;
-%Kirmizi icin 1, Yesil icin 2, Mavi icin 3.
 
 rgb = imread(ImPath);
 rgbSonuc = rgb;
 rgbEnc = Encyrpter(rgb(:, :, renk), data);
 
 rgbSonuc(:, :, renk) = rgbEnc;
-imwrite(rgbSonuc, stImPath, 'jpg');
+imwrite(rgbSonuc, stImPath, 'png');
 
 %Veriyi renkli resme nasil gomecegimizi daha sonra belirleyecegiz.
 
